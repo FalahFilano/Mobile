ee5936a49b555a7daf9a0a07b07e28af4f5a35c1
Unknown <falahnurli10@gmail.com>
2019-09-19 15:36:30 +0700

    add functionality

--- Notes/app/src/main/java/com/filano/notes/NewNote.java
+++ Notes/app/src/main/java/com/filano/notes/NewNote.java
@@ -3,16 +3,17 @@ package com.filano.notes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class NewNote extends AppCompatActivity {
 
     private Button btnDiscard, btnSave;
     private EditText etJudul, etNote;
-
-    private String judul, note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +25,29 @@ public class NewNote extends AppCompatActivity {
         btnDiscard = findViewById(R.id.btnDIscard);
         btnSave = findViewById(R.id.btnSave);
 
-        judul = etJudul.toString();
-        note = etNote.toString();
 
         btnDiscard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(NewNote.this, MainActivity.class);
                 startActivity(i);
+                finish();
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(NewNote.this, MainActivity.class);
+                Bundle extras = new Bundle();
+                extras.putString("head",etJudul.getText().toString());
+                extras.putString("note", etNote.getText().toString());
+                i.putExtras(extras);
+                startActivity(i);
+                finish();
             }
         });
 
     }
+
 }
--- Notes/app/src/main/java/com/filano/notes/NoteAdapter.java
+++ Notes/app/src/main/java/com/filano/notes/NoteAdapter.java
@@ -1,20 +1,56 @@
 package com.filano.notes;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter> {
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
 
     private ArrayList<NoteItem> itemList;
 
-    HomeAdapter(Context context, ArrayList<NoteItem> list){
-        mContext =context;
-        mlist = list;
+    public NoteAdapter( ArrayList<NoteItem> itemList){
+        this.itemList = itemList;
     }
 
-    public void setItemList(ArrayList<NoteActive> itemList){
-        this.itemList=
+    @NonNull
+    @Override
+    public NoteAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.note_item, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteAdapter.ViewHolder viewHolder, int i) {
+        NoteItem item =itemList.get(i);
+
+        viewHolder.tvJudul.setText(item.getJudul());
+        viewHolder.tvNote.setText(item.getNote());
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList != null ? itemList.size() : 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView tvJudul, tvNote;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvJudul= itemView.findViewById(R.id.tvJudul);
+            tvNote = itemView.findViewById(R.id.tvNote);
+
+        }
     }
 }
